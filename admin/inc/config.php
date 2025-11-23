144c51810bbcb39ea4086e0414edb9fc661eaa60
thuyngan05 <ngantran.31231023211@st.ueh.edu.vn>
2025-11-23 18:50:18 +0700

    fix: thêm port=3306 trong  admin/config.php

--- admin/inc/config.php
+++ admin/inc/config.php
@@ -11,7 +11,7 @@ define("BASE_URL", "");
 define("ADMIN_URL", BASE_URL . "admin/");
 
 try {
-    $pdo = new PDO("mysql:host={$dbhost};dbname={$dbname};charset=utf8mb4", $dbuser, $dbpass);
+    $pdo = new PDO("mysql:host={$dbhost};port=3306;dbname={$dbname};charset=utf8mb4", $dbuser, $dbpass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $exception) {
     echo "Connection error: " . $exception->getMessage();
