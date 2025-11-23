8a5ef0a2bfb7773a9fd9557c39aea3499bae21f1
thuyngan05 <ngantran.31231023211@st.ueh.edu.vn>
2025-11-23 19:15:01 +0700

    commit lan 2

--- admin/inc/config.php
+++ admin/inc/config.php
@@ -2,6 +2,7 @@
 ini_set('error_reporting', E_ALL);
 date_default_timezone_set('Asia/Ho_Chi_Minh');
 
+// Thuy Ngan test merge pull request 
 $dbhost = 'localhost';
 $dbname = 'ecommerceweb';
 $dbuser = 'root';
